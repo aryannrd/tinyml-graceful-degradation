714794c2e0e1188e16de3a921545b40d44bfd023
Aryan Dubey <aryandubey@umass.edu>
2026-09-22 15:25:24 -0400

    Add makefile and main.c to compile parse_csv and load test samples in a fixed array

---
+++ edge_nodes/include/main.h
@@ -0,0 +1,4 @@
+#ifndef MAIN_H
+#define MAIN_H
+int parse_csv(const char *path, float features[][4], int *labels, int max_rows);
+#endif
--- edge_nodes/src/main.c
+++ edge_nodes/src/main.c
@@ -1,4 +1,17 @@
+#include "../include/main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#define MAX_SAMPLES 300
+#define NUM_FEATURES 4
+float features[MAX_SAMPLES][NUM_FEATURES];
+int labels[MAX_SAMPLES];
 
-int main() {}
+int main() {
+  int count =
+      parse_csv("/Users/aryan/PycharmProjects/tinyml-graceful-degradation/"
+                "backup_data/test_features.csv",
+                features, labels, MAX_SAMPLES);
+  printf("Successfully loaded %d test samples.\n", count);
+}
